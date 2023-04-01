f5d70d4f562cbbdcb414c47060d8f43a87f86c0c
ddbrn <ppq0qqp@gmail.com>
2023-04-01 21:57:36 +0200

    pierwszy commit, szkielet projektu, chyba gotowe api?, commit do testowania zapytan do api na froncie z rzeczywistym czasem oczekiwania

---
+++ models/Recipe.js
@@ -0,0 +1,65 @@
+import { Schema, model, models } from 'mongoose';
+import mongooseSlugPlugin from 'mongoose-slug-plugin';
+import limax from 'limax';
+
+const recipeSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Recipe must have a name'],
+  },
+  stages: [
+    {
+      title: {
+        type: String,
+      },
+      ingredients: [
+        {
+          type: String,
+        },
+      ],
+      preparing: {
+        type: String,
+      },
+    },
+  ],
+  images: [
+    {
+      original: {
+        type: String,
+      },
+      thumbnail: {
+        type: String,
+      },
+    },
+  ],
+  // TODO dodac custom error message
+  category: {
+    type: String,
+    enum: [
+      'ciasta',
+      'slodkie',
+      'salatki',
+      'soki',
+      'obiadowe',
+      'przetwory',
+      'ryby',
+      'drinki',
+      'lody',
+      'fastfood',
+      'inne',
+    ],
+    required: [true, 'Recipe must have a category'],
+  },
+});
+
+recipeSchema.set('timestamps', true);
+
+// monguse-slug-plugin
+recipeSchema.plugin(mongooseSlugPlugin, {
+  errorMessage: 'slug errrr',
+  tmpl: '<%=name%>',
+  slug: limax,
+});
+// recipeSchema.plugin(mongoosePaginate);
+
+export default models.Recipe || model('Recipe', recipeSchema);
---
+++ pages/api/index.js
@@ -0,0 +1,3 @@
+export default (req, res) => {
+  res.status(200).json('TODO fajna strona powitalna do api');
+};
