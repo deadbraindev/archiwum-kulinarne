ccbef525087f58df045c62dea10c906628f02c60
ddbrn <ppq0qqp@gmail.com>
2023-05-26 19:03:24 +0200

    metatags test, favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -30,11 +30,33 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pl">
       <Head>
-        <meta name="theme-color" content="#ffce06" />
+        {/* <meta name="theme-color" content="#ffce06" />
         <meta name="apple-mobile-web-app-title" content="archiwum kulinarne" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico" sizes="any" /> */}
         {/* <link rel="manifest" href="/manifest.json" /> */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        {/* <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        /> */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffce06" />
+        <meta name="msapplication-TileColor" content="#ffce06" />
+        <meta name="theme-color" content="#ffce06" />
       </Head>
       <body>
         <div className="content">
