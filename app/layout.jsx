fa94252bf4ee9d93b8643fce345f37d6f890dbe5
ddbrn <ppq0qqp@gmail.com>
2023-06-07 18:05:42 +0200

    add vercel analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,6 @@
 // import Link from 'next/link';
 // import Head from 'next/head';
-
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/dist/react';
 
 import ReactQueryWrapper from '../components/ReactQueryWrapper';
 import Navbar from '../components/Navbar';
