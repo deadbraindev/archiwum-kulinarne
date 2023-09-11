ff96b6cc2e32b4dbf605fa250dac4e3ea3d6ca5d
ddbrn <ppq0qqp@gmail.com>
2023-09-11 20:46:32 +0200

    /losowy opengraph tags fix

---
+++ app/losowy/layout.jsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: 'losowy przepis | archiwum kulinarne',
+  openGraph: {
+    title: 'losowy przepis',
+    url: '/losowy',
+    images: [
+      {
+        url: 'https://archiwum-kulinarne.vercel.app/images/opengraph-img-1200-630.jpg',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+};
+
+export default function RootLayout({ children }) {
+  return <div>{children}</div>;
+}
