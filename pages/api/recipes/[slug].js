7e2e15bc7ed7d788e51947444c47b77ca2ae2370
ddbrn <ppq0qqp@gmail.com>
2024-01-03 23:35:04 +0100

    fix: adding new origin in allow list in cors in /recipes/[slug]

--- pages/api/recipes/[slug].js
+++ pages/api/recipes/[slug].js
@@ -21,6 +21,7 @@ export default async function handler(req, res) {
             'https://archiwumkulinarne.deadbrain.dev',
             'http://localhost:3000',
             'https://archiwumkulinarne.vercel.app',
+            'https://archiwumkulinarne.netlify.app',
           ],
           optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
         });
