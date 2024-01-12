0b0257bbb516aacef7d70e2083b63b2fe09ffcc2
ddbrn <ppq0qqp@gmail.com>
2024-01-12 21:56:50 +0100

    feat: add WPA section, tabs

--- app/wpa/page.jsx
+++ app/wpa/page.jsx
@@ -1,18 +1,7 @@
-export const metadata = {
-  title: 'wpa | archiwum kulinarne',
-  openGraph: {
-    title: 'wpa',
-    url: '/wpa',
-    images: [
-      {
-        url: 'https://archiwum-kulinarne.vercel.app/images/opengraph-img-1200-630.jpg',
-        width: 1200,
-        height: 630,
-      },
-    ],
-  },
-};
+'use client';
+
+import WPATabs from '../../components/WPATabs';
 
 export default function Page() {
-  return <div className="footer">WPA</div>;
+  return <WPATabs />;
 }
