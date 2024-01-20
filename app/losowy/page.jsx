6e59b7d000500b83ea3daa59b26efb287b34f018
ddbrn <ppq0qqp@gmail.com>
2024-01-20 18:15:25 +0100

    fix: fixing getRecipe in /losowy

--- app/losowy/page.jsx
+++ app/losowy/page.jsx
@@ -6,10 +6,12 @@ import getRecipe from '../../lib/getRecipe';
 import RecipeCardSkeleton from '../../components/RecipeCardSkeleton';
 
 export default function Page() {
-  // function Losowy() {
   const router = useRouter();
   const fetchRandomRecipe = async () => {
-    const recipe = await getRecipe('random');
+    const recipe = await getRecipe([
+      'https://archiwumkulinarne.deadbrain.dev/api/recipes',
+      'random',
+    ]);
     router.replace(`/przepisy/${recipe.slug.slugCurrent}`);
   };
 
